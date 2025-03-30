8ee179a4752d8276b7779d6ecbca59797283c49d
renaise <krenaise@hotmail.com>
2025-03-30 18:53:49 -0400

    Adjusted mobile leading for better readability

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,6 @@
               <div className="md:col-start-2">
                 <div className="font-medium">
-                  <p className="text-sm md:text-sm font-['Helvetica_Now',Helvetica,Arial,sans-serif] leading-[60%] tracking-[-1px] mb-6">
+                  <p className="text-sm md:text-sm font-['Helvetica_Now',Helvetica,Arial,sans-serif] leading-[90%] md:leading-[60%] tracking-[-1px] mb-6">
                     <span className="text-white">ARTIFICE</span> is a production studio and platform showcasing <span className="text-white">art, technology, sound, and design</span>. 
                     We foster a cross-disciplinary exchange where material and immaterial (digital) artifacts co-exist, encouraging new ways of engaging with creative work. 
                     Our <span className="text-white">WHITE BOX</span> (gallery) and <span className="text-white">BLACK BOX</span> (media space) is intended to cultivate collective engagement and community-driven artistic exploration.
